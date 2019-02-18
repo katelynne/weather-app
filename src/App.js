0d7d089f4f77e10690a3c8e94aca833701cc66e8
David Moore <KateandDave@Davids-MacBook-Air.local>
2019-02-18 13:00:07 -0500

    weather api working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+import Titles from "./Components/Titles";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+const API_KEY = "38d36588c8478532e1570e211c38464d";
+
+
+class App extends React.Component{
+
+  state = {
+    teperature : undefined,
+    city: undefined,
+    country: undefined,
+    humitity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    if(city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+     });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Enter Values"
+     });
+    }
+  }
+  render(){
+    return(
+    <div>
+      <Titles />
+      <Form getWeather={this.getWeather}/>
+      <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+      />
+    </div>
     );
   }
-}
-
+};
 export default App;
